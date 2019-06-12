d86c18dc00250eac76478da715661839d56bca68
mais <380354618@qq.com>
2019-06-12 16:30:42 +0800

    first

---
+++ demo.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once "./vendor/autoload.php";
+
+use Compo\Starfish\Tools;
+
+Tools::Hello();
---
+++ src/Compo/Starfish/Tools.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/6/12
+ * Time: 16:13
+ */
+namespace Compo\Starfish;
+
+class Tools
+{
+    public function __construct()
+    {
+    }
+
+    public static function Hello ()
+    {
+        echo 'helowwwww';
+    }
+}
